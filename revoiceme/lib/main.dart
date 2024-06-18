8ee1157b6c2ab269b46d36c6681849083c5ede0a
smartin314 <164185529+smartin314@users.noreply.github.com>
2024-06-18 13:51:32 +0200

    feat: Merge changing volume with playing input at the same time
    Merge the branch 5 with branch 4 in dev.
    
    -

--- revoiceme/lib/main.dart
+++ revoiceme/lib/main.dart
@@ -1,29 +1,46 @@
-
 import 'dart:async';
 
 import 'package:flutter/material.dart';
 import 'package:flutter_sound/flutter_sound.dart';
 import 'package:permission_handler/permission_handler.dart';
+import 'package:flutter/foundation.dart';
+import 'package:audioplayers/audioplayers.dart';
+import 'package:volume_controller/volume_controller.dart';
 
 void main() {
   runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: Scaffold(
-        appBar: AppBar(
-          title: Text('Real-time Voice Amplifier'),
-        ),
-        body: VoiceAmplifier(),
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
       ),
+      home: const VoiceAmplifier(title: 'Flutter Demo Home Page'),
     );
   }
 }
 
 class VoiceAmplifier extends StatefulWidget {
+  const VoiceAmplifier({super.key, required this.title});
+
+  // This widget is the home page of your application. It is stateful, meaning
+  // that it has a State object (defined below) that contains fields that affect
+  // how it looks.
+
+  // This class is the configuration for the state. It holds the values (in this
+  // case the title) provided by the parent (in this case the App widget) and
+  // used by the build method of the State. Fields in a Widget subclass are
+  // always marked "final".
+
+  final String title;
+
   @override
   _VoiceAmplifierState createState() => _VoiceAmplifierState();
 }
@@ -32,10 +49,22 @@ class _VoiceAmplifierState extends State<VoiceAmplifier> {
   final FlutterSoundPlayer _player = FlutterSoundPlayer();
   final FlutterSoundRecorder _recorder = FlutterSoundRecorder();
   bool _isRecording = false;
+  bool _isToggled = false;
+  double _currentVolume = 0.5;
 
   @override
   void initState() {
     super.initState();
+    VolumeController().listener((volume) {
+      if (kDebugMode) {
+        print('Volume changed: $volume');
+      }
+      setState(() {
+        _currentVolume = volume;
+      });
+    });
+    VolumeController().showSystemUI = true;
+    _initVolume();
     _initializeAudioSessions();
   }
 
@@ -54,7 +83,68 @@ class _VoiceAmplifierState extends State<VoiceAmplifier> {
   void dispose() {
     _player.closePlayer();
     _recorder.closeRecorder();
+    VolumeController().removeListener();
     super.dispose();
+  }
+
+  Future<void> _initVolume() async {
+    double volume = await VolumeController().getVolume();
+    setState(() {
+      _currentVolume = volume;
+    });
+  }
+
+  Future<void> _showWarningMaxVolume() async {
+    showDialog<void>(
+      context: context,
+      barrierDismissible: true, // user must tap button!
+      builder: (BuildContext context) {
+        return AlertDialog(
+          title: const Text('Maximale Lautstärke erreicht'),
+          content: const Text(
+              'Stellen Sie sicher, dass der Lautsprecher auf voller Laustärke eingestellt ist.'),
+          actions: <Widget>[
+            TextButton(
+              child: const Text('Ok'),
+              onPressed: () {
+                Navigator.of(context).pop();
+              },
+            ),
+          ],
+        );
+      },
+    );
+  }
+
+  void _toggleSlider() {
+    setState(() {
+      _isToggled = !_isToggled;
+    });
+  }
+
+  void _changeVolume(double volume) async {
+    if (volume < 0.0) {
+      return;
+    } else if (volume > 1.0) {
+      await _showWarningMaxVolume();
+      return;
+    }
+    setState(() {
+      _currentVolume = volume;
+    });
+    VolumeController().setVolume(volume);
+  }
+
+  void _increaseVolume() {
+    if (_currentVolume < 1.0) {
+      _changeVolume((_currentVolume + 0.1));
+    }
+  }
+
+  void _decreaseVolume() {
+    if (_currentVolume > 0.0) {
+      _changeVolume((_currentVolume - 0.1));
+    }
   }
 
   Future<void> _startAmplifying() async {
@@ -99,15 +189,77 @@ class _VoiceAmplifierState extends State<VoiceAmplifier> {
 
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: <Widget>[
-          ElevatedButton(
-            onPressed: _isRecording ? _stopAmplifying : _startAmplifying,
-            child: Text(_isRecording ? 'Stop Amplifying' : 'Start Amplifying'),
-          ),
-        ],
+    return Scaffold(
+      appBar: AppBar(title: const Text('ReVoiceMe')),
+      body: Center(
+        // Center is a layout widget. It takes a single child and positions it
+        // in the middle of the parent.
+        child: Column(
+            // Column is also a layout widget. It takes a list of children and
+            // arranges them vertically. By default, it sizes itself to fit its
+            // children horizontally, and tries to be as tall as its parent.
+            //
+            // Column has various properties to control how it sizes itself and
+            // how it positions its children. Here we use mainAxisAlignment to
+            // center the children vertically; the main axis here is the vertical
+            // axis because Columns are vertical (the cross axis would be
+            // horizontal).
+            //
+            // TRY THIS: Invoke "debug painting" (choose the "Toggle Debug Paint"
+            // action in the IDE, or press "p" in the console), to see the
+            // wireframe for each widget.
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              ElevatedButton(
+                onPressed: _isRecording ? _stopAmplifying : _startAmplifying,
+                child:
+                    Text(_isRecording ? 'Stop Amplifying' : 'Start Amplifying'),
+              ),
+              Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 50.0),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.start,
+                  children: [
+                    const Text(
+                      'Lautstärke ',
+                      style: TextStyle(
+                        fontSize: 24, // Adjust this value based on switch size
+                      ),
+                    ),
+                    Switch(
+                      value: _isToggled,
+                      activeColor: Colors.green,
+                      onChanged: (bool value) {
+                        // This is called when the user toggles the switch.
+                        setState(() {
+                          _isToggled = value;
+                        });
+                      },
+                    ),
+                  ],
+                ),
+              ),
+              if (_isToggled)
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    IconButton(
+                      icon: Icon(Icons.remove),
+                      onPressed: _decreaseVolume,
+                    ),
+                    Slider(
+                      value: _currentVolume,
+                      onChanged: _changeVolume,
+                      min: 0.0,
+                      max: 1.0,
+                    ),
+                    IconButton(
+                      icon: Icon(Icons.add),
+                      onPressed: _increaseVolume,
+                    ),
+                  ],
+                ),
+            ]),
       ),
     );
   }
