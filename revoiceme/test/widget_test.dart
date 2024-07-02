fbca1e02ff59016790a281dc77e647725dc1006f
L483 <55800398+L483@users.noreply.github.com>
2024-07-02 18:07:54 +0200

    test: apply renaming to tests

--- revoiceme/test/widget_test.dart
+++ revoiceme/test/widget_test.dart
@@ -13,7 +13,7 @@ import "package:revoiceme/main.dart";
 void main() {
   testWidgets("Counter increments smoke test", (WidgetTester tester) async {
     // Build our app and trigger a frame.
-    await tester.pumpWidget(const MyApp()); //const MyApp()
+    await tester.pumpWidget(const ReVoiceMe()); //const MyApp()
 
     // Verify that our counter starts at 0.
     expect(find.text("0"), findsOneWidget);
