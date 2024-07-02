d80bb76e51f9d416ce2cc467787fc6de98c9fd5f
L483 <55800398+L483@users.noreply.github.com>
2024-07-02 18:12:56 +0200

    docs(main): remove unnecessary comments

--- revoiceme/lib/main.dart
+++ revoiceme/lib/main.dart
@@ -27,18 +27,10 @@ class ReVoiceMe extends StatelessWidget {
 }
 
 class VoiceAmplifier extends StatefulWidget {
-  const VoiceAmplifier({required this.title, super.key});
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
+  /// Creates a new [VoiceAmplifier] instance.
+  const VoiceAmplifier({
+    super.key,
+  });
 
   @override
   State<VoiceAmplifier> createState() => _VoiceAmplifierState();
@@ -161,22 +153,7 @@ class _VoiceAmplifierState extends State<VoiceAmplifier> {
   Widget build(BuildContext context) => Scaffold(
         appBar: AppBar(title: const Text("ReVoiceMe")),
         body: Center(
-          // Center is a layout widget. It takes a single child and positions it
-          // in the middle of the parent.
           child: Column(
-            // Column is also a layout widget. It takes a list of children and
-            // arranges them vertically. By default, it sizes itself to fit its
-            // children horizontally, and tries to be as tall as its parent.
-            //
-            // Column has various properties to control how it sizes itself and
-            // how it positions its children. Here we use mainAxisAlignment to
-            // center the children vertically; the main axis here is the vertical
-            // axis because Columns are vertical (the cross axis would be
-            // horizontal).
-            //
-            // TRY THIS: Invoke "debug painting" (choose the "Toggle Debug Paint"
-            // action in the IDE, or press "p" in the console), to see the
-            // wireframe for each widget.
             mainAxisAlignment: MainAxisAlignment.center,
             children: <Widget>[
               Padding(
