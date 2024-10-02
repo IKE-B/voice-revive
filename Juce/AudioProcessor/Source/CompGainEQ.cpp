8a40efe9a1de0faa958a40bcfc5b19adc99f19fe
smartin314 <164185529+smartin314@users.noreply.github.com>
2024-10-02 09:57:02 +0200

    fix: delete unnecessary methods

--- Juce/AudioProcessor/Source/CompGainEQ.cpp
+++ Juce/AudioProcessor/Source/CompGainEQ.cpp
@@ -580,18 +580,3 @@ void CompGainEQ::updatePeakFilter(const ChainSettings& chainSettings)
     updateCoefficients(leftChain.get<ChainPositions::Peak>().coefficients, peakCoefficients);
     updateCoefficients(rightChain.get<ChainPositions::Peak>().coefficients, peakCoefficients);
 }
-
-//==============================================================================
-void CompGainEQ::getStateInformation(juce::MemoryBlock& destData)
-{
-    // You should use this method to store your parameters in the memory block.
-    // You could do that either as raw data, or use the XML or ValueTree classes
-    // as intermediaries to make it easy to save and load complex data.
-    juce::MemoryOutputStream mos(destData, true);
-}
-
-void CompGainEQ::setStateInformation(const void* data, int sizeInBytes)
-{
-    // You should use this method to restore your parameters from this memory block,
-    // whose contents will have been created by the getStateInformation() call.
-}
