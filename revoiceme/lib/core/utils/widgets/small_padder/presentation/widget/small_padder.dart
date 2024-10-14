157012a5a7fffcdb2f8c32a3c39655bbcadb204d
L483 <55800398+L483@users.noreply.github.com>
2024-10-14 11:14:34 +0200

    style(small_padder): auto-format code

--- revoiceme/lib/core/utils/widgets/small_padder/presentation/widget/small_padder.dart
+++ revoiceme/lib/core/utils/widgets/small_padder/presentation/widget/small_padder.dart
@@ -2,13 +2,15 @@ import "package:flutter/material.dart";
 import "package:revoiceme/core/utils/styles/default_theme.dart";
 
 /// A widget to apply uniformly sized padding in the app where needed.
-/// 
+///
 /// {@category Widget}
 class SmallPadder extends StatelessWidget {
   /// Creates a new [SmallPadder] instance.
   const SmallPadder({super.key});
 
   @override
-  Widget build(BuildContext context) =>
-      const SizedBox(width: CustomThemeData.smallPadding, height: CustomThemeData.smallPadding);
+  Widget build(BuildContext context) => const SizedBox(
+        width: CustomThemeData.smallPadding,
+        height: CustomThemeData.smallPadding,
+      );
 }
