b4f7286aa67d4fdda8b4ff68a118948340277729
L483 <55800398+L483@users.noreply.github.com>
2024-10-14 11:26:08 +0200

    feat(voice_changing_demo): use global bloc for controlling voice changing

--- revoiceme/lib/core/utils/widgets/revoiceme_appbar/presentation/widget/revoiceme_appbar.dart
+++ revoiceme/lib/core/utils/widgets/revoiceme_appbar/presentation/widget/revoiceme_appbar.dart
@@ -1,69 +1,89 @@
 import "package:flutter/material.dart";
+import "package:flutter_bloc/flutter_bloc.dart";
+import "package:revoiceme/core/routers/router.dart";
 import "package:revoiceme/core/utils/styles/default_theme.dart";
+import "package:revoiceme/core/utils/widgets/voice_change_demo/presentation/widget/voice_change_demo_bloc.dart";
+import "package:revoiceme/core/utils/widgets/voice_change_demo/presentation/widget/voice_change_demo_event.dart";
+import "package:revoiceme/core/utils/widgets/voice_change_demo/presentation/widget/voice_change_demo_state.dart";
 
 /// The app bar for the ReVoiceMe app.
 ///
+/// Depends on [VoiceChangeDemo].
+/// The [ReVoiceMeAppBar] is a custom app bar consisting of multiple components.
+/// The title is automatically inferred from the current route.
 /// The app bar provides an icon button to toggle voice changing.
-/// The icon changes depending on whether voice changing is applied.
+/// The icon changes depending on whether
+/// the user is allowed to toggle the voice changing and whether voice changing is active.
 ///
 /// {@category Widget}
+/// {@category Global}
 class ReVoiceMeAppBar extends StatelessWidget implements PreferredSizeWidget {
   /// Creates a new [ReVoiceMeAppBar] instance.
   const ReVoiceMeAppBar({
-    required this.title,
-    required this.userHasControl,
-    required this.appliesVoiceChanging,
-    required this.toggleVoiceChanging,
     super.key,
   });
-
-  /// The text to display in the app bar.
-  final String title;
-
-  /// Whether the user currently can toggle the voice changing.
-  final bool userHasControl;
-
-  /// Whether the voice changing is currently active.
-  final bool appliesVoiceChanging;
-
-  /// The function to call for toggling the voice changing.
-  final VoidCallback toggleVoiceChanging;
 
   /// Change the app bar height to be more visually appealing.
   @override
   Size get preferredSize => const Size.fromHeight(kToolbarHeight);
 
+  /// Infer the title of the app bar from the current route.
+  String inferTitle(BuildContext context) => routes.values
+      .firstWhere(
+        (({IconData icon, String path, String text}) element) =>
+            element.path == ModalRoute.of(context)?.settings.name,
+      )
+      .text;
+
+  /// Infer the icon to show for indicating the voice changing state.
+  IconData inferIndicatorIcon(VoiceChangeDemoState state) =>
+      state.isChangingVoice ? Icons.mic : Icons.mic_off;
+
+  /// Function to call when the microphone icon is pressed.
+  /// Attempts to toggle the voice changing state depending on whether
+  /// the user is allowed to toggle the voice changing and whether voice changing is active.
+  void onPressed(BuildContext context, VoiceChangeDemoState state) {
+    if (state.isToggleAllowed) {
+      if (state.isChangingVoice) {
+        context.read<VoiceChangeDemo>().add(StopChanging());
+      } else {
+        context.read<VoiceChangeDemo>().add(StartChanging());
+      }
+    }
+  }
+
+  /// Infer the color of the indicator depending on
+  /// whether the user is allowed to toggle the voice changing.
+  Color inferIndicatorColor(BuildContext context, VoiceChangeDemoState state) {
+    final ColorScheme colors = Theme.of(context).colorScheme;
+    return state.isToggleAllowed
+        ? colors.onSurface
+        : Theme.of(context).disabledColor;
+  }
+
   @override
   Widget build(BuildContext context) {
-    // Get icons
-    const IconData activeIcon = Icons.mic;
-    const IconData inactiveIcon = Icons.mic_off;
-
-    // Get colors
+    // Get color palette
     final ColorScheme colors = Theme.of(context).colorScheme;
-    final Color enabledColor = colors.onSurface;
-    final Color disabledColor = Theme.of(context).disabledColor;
 
-    return AppBar(
-      title: Text(title),
-      backgroundColor: colors.primary,
-      actions: <Widget>[
-        IconButton(
-          icon: Icon(
-            appliesVoiceChanging ? activeIcon : inactiveIcon,
+    return BlocBuilder<VoiceChangeDemo, VoiceChangeDemoState>(
+      builder: (BuildContext context, VoiceChangeDemoState state) => AppBar(
+        title: Text(inferTitle(context)),
+        backgroundColor: colors.primary,
+        actions: <Widget>[
+          IconButton(
+            icon: Icon(inferIndicatorIcon(state)),
+            onPressed: () => onPressed(context, state),
           ),
-          onPressed: () {
-            if (userHasControl) toggleVoiceChanging();
-          },
+        ],
+        iconTheme: IconThemeData(
+          color: colors.onSurface,
+          size: CustomThemeData.defaultIconSize,
         ),
-      ],
-      iconTheme: IconThemeData(
-        color: colors.onSurface,
-        size: CustomThemeData.defaultIconSize,
-      ),
-      actionsIconTheme: IconThemeData(
-        color: userHasControl ? enabledColor : disabledColor,
-        size: CustomThemeData.defaultIconSize,
+        actionsIconTheme: IconThemeData(
+          color: inferIndicatorColor(context, state),
+          size: CustomThemeData.defaultIconSize,
+        ),
       ),
     );
   }
--- revoiceme/lib/screens/main/presentation/screen/home_screen.dart
+++ revoiceme/lib/screens/main/presentation/screen/home_screen.dart
@@ -1,12 +1,9 @@
-import "dart:async";
 import "package:flutter/material.dart";
-import "package:flutter_sound/flutter_sound.dart";
-import "package:permission_handler/permission_handler.dart";
-import "package:revoiceme/core/routers/router.dart";
+import "package:revoiceme/core/utils/widgets/device_volume_slider/presentation/widget/device_volume_slider.dart";
+import "package:revoiceme/core/utils/widgets/play_button/presentation/widget/play_button.dart";
 import "package:revoiceme/core/utils/widgets/revoiceme_appbar/presentation/widget/revoiceme_appbar.dart";
 import "package:revoiceme/core/utils/widgets/revoiceme_drawer/presentation/widget/revoiceme_drawer.dart";
 import "package:revoiceme/core/utils/widgets/small_padder/presentation/widget/small_padder.dart";
-import "package:revoiceme/core/utils/widgets/voice_changer_control/presentation/widget/voice_changer_control.dart";
 import "package:revoiceme/core/utils/widgets/voice_changer_slider/presentation/widget/voice_changer_slider.dart";
 
 // TODO(L483): Integrate the app with the real voice changing feature.
@@ -45,15 +42,7 @@ class _HomeScreenState extends State<HomeScreen> {
           child: Column(
             mainAxisAlignment: MainAxisAlignment.center,
             children: <Widget>[
-              VoiceChangerControl(
-                toggleVoiceChanging:
-                    _isAmplifying ? _stopAmplifying : _startAmplifying,
-                userHasControl: true,
-                appliesVoiceChanging: _isAmplifying,
-                size: 80,
-                activeIcon: Icons.pause_rounded,
-                inactiveIcon: Icons.play_arrow_rounded,
-              ),
+              const PlayButton(size: 80),
               const SmallPadder(),
               const DeviceVolumeSlider(),
               ValueListenableBuilder<double>(
