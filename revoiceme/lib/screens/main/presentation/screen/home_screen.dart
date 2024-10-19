70882eed096f5eeafb9a5e8c5d4b07d52a1b28ed
L483 <55800398+L483@users.noreply.github.com>
2024-10-19 10:52:19 +0200

    feat(home_screen): remove stub slider

--- revoiceme/lib/screens/main/presentation/screen/home_screen.dart
+++ revoiceme/lib/screens/main/presentation/screen/home_screen.dart
@@ -4,7 +4,6 @@ import "package:revoiceme/core/utils/widgets/play_button/presentation/widget/pla
 import "package:revoiceme/core/utils/widgets/revoiceme_appbar/presentation/widget/revoiceme_appbar.dart";
 import "package:revoiceme/core/utils/widgets/revoiceme_drawer/presentation/widget/revoiceme_drawer.dart";
 import "package:revoiceme/core/utils/widgets/small_padder/presentation/widget/small_padder.dart";
-import "package:revoiceme/core/utils/widgets/voice_changer_slider/presentation/widget/voice_changer_slider.dart";
 
 // TODO(L483): Integrate the app with the real voice changing feature.
 
@@ -22,46 +21,27 @@ class HomeScreen extends StatefulWidget {
 }
 
 class _HomeScreenState extends State<HomeScreen> {
-  /// Listens to changes in the pitch parameter for the voice changer. Only a stub for now.
-  ValueNotifier<double> pitch = ValueNotifier<double>(1.5);
-
   @override
   void dispose() {
     // cleanup value notifiers
-    pitch.dispose();
     super.dispose();
   }
 
   @override
-  Widget build(BuildContext context) => Scaffold(
-        appBar: const ReVoiceMeAppBar(),
-        drawer: const ReVoiceMeDrawer(
+  Widget build(BuildContext context) => const Scaffold(
+        appBar: ReVoiceMeAppBar(),
+        drawer: ReVoiceMeDrawer(
           cleanup: DeviceVolumeSlider.cleanup,
         ),
         body: Center(
           child: Column(
             mainAxisAlignment: MainAxisAlignment.center,
             children: <Widget>[
-              const PlayButton(size: 80),
-              const SmallPadder(),
-              const DeviceVolumeSlider(),
-              ValueListenableBuilder<double>(
-                valueListenable: pitch,
-                builder: (BuildContext context, double value, Widget? child) =>
-                    VoiceChangerSlider(
-                  title: "Tonhöhe",
-                  value: value,
-                  min: -50,
-                  max: 50,
-                  delta: 5.0,
-                  onChanged: (double value) {
-                    debugPrint("$pitch");
-                    debugPrint("$value");
-                    pitch.value = value;
-                  },
-                  togglable: true,
-                ),
-              ),
+              PlayButton(size: 80),
+              SmallPadder(),
+              SmallPadder(),
+              SmallPadder(),
+              DeviceVolumeSlider(),
             ],
           ),
         ),
