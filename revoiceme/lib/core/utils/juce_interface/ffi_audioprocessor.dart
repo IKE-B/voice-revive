8d587b5b5c6542884dac5061a13ca11ac61b08e3
Lukas Wagner <lukas.wagner.ext@zeiss.com>
2024-10-19 15:58:32 +0200

    add interface file to call and lookup juce library functions

--- revoiceme/lib/core/utils/juce_interface/ffi_audioprocessor.dart
+++ revoiceme/lib/core/utils/juce_interface/ffi_audioprocessor.dart
@@ -1,3 +1,33 @@
-import "dart:ffi";
-import "package:ffi/ffi.dart";
+import "dart:ffi" as ffi;
+import 'dart:io' show Platform;
+
+typedef startModulationFunctionCPP = ffi.Void Function();
+typedef startModulationFunctionDart = void Function();
+typedef stopModulationFunctionCPP = ffi.Void Function();
+typedef stopModulationFunctionDart = void Function();
+typedef updateValuesFunctionCPP = ffi.Void Function(ffi.Float, ffi.Float, ffi.Float, ffi.Float, ffi.Float, ffi.Bool, ffi.Bool,
+                                                ffi.Float, ffi.Float, ffi.Float, ffi.Float, ffi.Bool, ffi.Bool, ffi.Bool,
+                                                ffi.Float, ffi.Float, ffi.Float, ffi.Float, ffi.Bool, ffi.Bool, ffi.Bool,
+                                                ffi.Float, ffi.Float, ffi.Float, ffi.Float, ffi.Bool, ffi.Bool, ffi.Bool,
+                                                ffi.Float, ffi.Float, ffi.Float, ffi.Float, ffi.Float, ffi.Float, ffi.Float, ffi.Float);
+typedef updateValuesFunctionDart = void Function(double, double, double, double, double, bool, bool,
+                                                double, double, double, double, bool, bool, bool,
+                                                double, double, double, double, bool, bool, bool,
+                                                double, double, double, double, bool, bool, bool,
+                                                double, double, double, double, double, double, double, double);
+
+late final startModulation, stopModulation, updateValues;
+late final ffi.DynamicLibrary nativeLibrary;
+
+void _initLibrary() 
+{
+  nativeLibrary = Platform.isMacOS || Platform.isIOS
+        ? ffi.DynamicLibrary.process() // macos and ios
+        : (ffi.DynamicLibrary.open(Platform.isWindows // windows
+            ? 'ffi_dart_library.dll'
+            : 'libffi_dart_library.so'));
+  startModulation = nativeLibrary.lookupFunction<startModulationFunctionCPP, startModulationFunctionDart>('startModulation');
+  stopModulation = nativeLibrary.lookupFunction<stopModulationFunctionCPP, stopModulationFunctionDart>('stopModulation');
+  updateValues = nativeLibrary.lookupFunction<updateValuesFunctionCPP, updateValuesFunctionDart>('updateValues');
+}
 
