3a0ec157d40081cec740c66dea692191e1c94c35
L483 <55800398+L483@users.noreply.github.com>
2024-10-19 10:54:46 +0200

    feat(configurator_screen): add ui elements for remaining parameters and order them as wanted

--- revoiceme/lib/screens/main/presentation/screen/configurator_screen.dart
+++ revoiceme/lib/screens/main/presentation/screen/configurator_screen.dart
@@ -5,6 +5,7 @@ import "package:revoiceme/core/utils/styles/default_theme.dart";
 import "package:revoiceme/core/utils/widgets/device_volume_slider/presentation/widget/device_volume_slider.dart";
 import "package:revoiceme/core/utils/widgets/revoiceme_appbar/presentation/widget/revoiceme_appbar.dart";
 import "package:revoiceme/core/utils/widgets/revoiceme_drawer/presentation/widget/revoiceme_drawer.dart";
+import "package:revoiceme/core/utils/widgets/small_padder/presentation/widget/small_padder.dart";
 import "package:revoiceme/core/utils/widgets/voice_changer_slider/presentation/widget/voice_changer_slider.dart";
 
 // TODO(L483): Integrate the app with the real voice changing feature.
@@ -28,6 +29,159 @@ class _ConfiguratorScreenState extends State<ConfiguratorScreen> {
   @override
   void initState() {
     notifiers = initValueNotifiers();
+    // reorder notifiers, for maps the order is the order of insertion
+    // so to integrate the three original maps into each other and to reorder them as wanted
+    // the entries have to be removed and reinserted in the wanted order
+    notifiers.addEntries(<MapEntry<String, ValueNotifier<Object>>>[
+      MapEntry<String, ValueNotifier<Object>>(
+        "gain",
+        notifiers.remove("gain")!,
+      ),
+      MapEntry<String, ValueNotifier<Object>>(
+        "compAllAttack",
+        notifiers.remove("compAllAttack")!,
+      ),
+      MapEntry<String, ValueNotifier<Object>>(
+        "compAllRelease",
+        notifiers.remove("compAllRelease")!,
+      ),
+      MapEntry<String, ValueNotifier<Object>>(
+        "compAllThreshold",
+        notifiers.remove("compAllThreshold")!,
+      ),
+      MapEntry<String, ValueNotifier<Object>>(
+        "compAllRatio",
+        notifiers.remove("compAllRatio")!,
+      ),
+      MapEntry<String, ValueNotifier<Object>>(
+        "compAllBypassed",
+        notifiers.remove("compAllBypassed")!,
+      ),
+      MapEntry<String, ValueNotifier<Object>>(
+        "compAllMute",
+        notifiers.remove("compAllMute")!,
+      ),
+      MapEntry<String, ValueNotifier<Object>>(
+        "compLowAttack",
+        notifiers.remove("compLowAttack")!,
+      ),
+      MapEntry<String, ValueNotifier<Object>>(
+        "compLowRelease",
+        notifiers.remove("compLowRelease")!,
+      ),
+      MapEntry<String, ValueNotifier<Object>>(
+        "compLowThreshold",
+        notifiers.remove("compLowThreshold")!,
+      ),
+      MapEntry<String, ValueNotifier<Object>>(
+        "compLowRatio",
+        notifiers.remove("compLowRatio")!,
+      ),
+      MapEntry<String, ValueNotifier<Object>>(
+        "compLowBypassed",
+        notifiers.remove("compLowBypassed")!,
+      ),
+      MapEntry<String, ValueNotifier<Object>>(
+        "compLowMute",
+        notifiers.remove("compLowMute")!,
+      ),
+      MapEntry<String, ValueNotifier<Object>>(
+        "compLowSolo",
+        notifiers.remove("compLowSolo")!,
+      ),
+      MapEntry<String, ValueNotifier<Object>>(
+        "compMidAttack",
+        notifiers.remove("compMidAttack")!,
+      ),
+      MapEntry<String, ValueNotifier<Object>>(
+        "compMidRelease",
+        notifiers.remove("compMidRelease")!,
+      ),
+      MapEntry<String, ValueNotifier<Object>>(
+        "compMidThreshold",
+        notifiers.remove("compMidThreshold")!,
+      ),
+      MapEntry<String, ValueNotifier<Object>>(
+        "compMidRatio",
+        notifiers.remove("compMidRatio")!,
+      ),
+      MapEntry<String, ValueNotifier<Object>>(
+        "compMidBypassed",
+        notifiers.remove("compMidBypassed")!,
+      ),
+      MapEntry<String, ValueNotifier<Object>>(
+        "compMidMute",
+        notifiers.remove("compMidMute")!,
+      ),
+      MapEntry<String, ValueNotifier<Object>>(
+        "compMidSolo",
+        notifiers.remove("compMidSolo")!,
+      ),
+      MapEntry<String, ValueNotifier<Object>>(
+        "compHighAttack",
+        notifiers.remove("compHighAttack")!,
+      ),
+      MapEntry<String, ValueNotifier<Object>>(
+        "compHighRelease",
+        notifiers.remove("compHighRelease")!,
+      ),
+      MapEntry<String, ValueNotifier<Object>>(
+        "compHighThreshold",
+        notifiers.remove("compHighThreshold")!,
+      ),
+      MapEntry<String, ValueNotifier<Object>>(
+        "compHighRatio",
+        notifiers.remove("compHighRatio")!,
+      ),
+      MapEntry<String, ValueNotifier<Object>>(
+        "compHighBypassed",
+        notifiers.remove("compHighBypassed")!,
+      ),
+      MapEntry<String, ValueNotifier<Object>>(
+        "compHighMute",
+        notifiers.remove("compHighMute")!,
+      ),
+      MapEntry<String, ValueNotifier<Object>>(
+        "compHighSolo",
+        notifiers.remove("compHighSolo")!,
+      ),
+      MapEntry<String, ValueNotifier<Object>>(
+        "lowMidCutoff",
+        notifiers.remove("lowMidCutoff")!,
+      ),
+      MapEntry<String, ValueNotifier<Object>>(
+        "midHighCutoff",
+        notifiers.remove("midHighCutoff")!,
+      ),
+      MapEntry<String, ValueNotifier<Object>>(
+        "lowCutFreq",
+        notifiers.remove("lowCutFreq")!,
+      ),
+      MapEntry<String, ValueNotifier<Object>>(
+        "highCutFreq",
+        notifiers.remove("highCutFreq")!,
+      ),
+      MapEntry<String, ValueNotifier<Object>>(
+        "peakFreq",
+        notifiers.remove("peakFreq")!,
+      ),
+      MapEntry<String, ValueNotifier<Object>>(
+        "peakGainInDecibels",
+        notifiers.remove("peakGainInDecibels")!,
+      ),
+      MapEntry<String, ValueNotifier<Object>>(
+        "peakQuality",
+        notifiers.remove("peakQuality")!,
+      ),
+      MapEntry<String, ValueNotifier<Object>>(
+        "lowCutSlope",
+        notifiers.remove("lowCutSlope")!,
+      ),
+      MapEntry<String, ValueNotifier<Object>>(
+        "highCutSlope",
+        notifiers.remove("highCutSlope")!,
+      ),
+    ]);
     super.initState();
   }
 
@@ -133,6 +287,76 @@ class _ConfiguratorScreenState extends State<ConfiguratorScreen> {
     );
   }
 
+  /// Builds a dropdown for a parameter that is coppled with a [ValueNotifier].
+  /// The dropdown is configured as specified in the [juceDropdownParameters].
+  ValueListenableBuilder<double>? dropdownBuilder(String key) {
+    final ({double init, String title, List<double> values})? parameter =
+        juceDropdownParameters[key];
+    if (parameter == null) return null;
+    return ValueListenableBuilder<double>(
+      valueListenable: notifiers[key]! as ValueNotifier<double>,
+      builder: (BuildContext context, double value, Widget? child) => Container(
+        padding: const EdgeInsets.symmetric(
+          horizontal: CustomThemeData.defaultPadding,
+        ),
+        child: Row(
+          children: <Widget>[
+            Text(
+              "${parameter.title}:",
+              style: Theme.of(context).textTheme.bodyLarge,
+            ),
+            const SmallPadder(),
+            DropdownButton<double>(
+              value: value,
+              items: parameter.values
+                  .map(
+                    (double value) => DropdownMenuItem<double>(
+                      value: value,
+                      child: Text(value.toStringAsFixed(1)),
+                    ),
+                  )
+                  .toList(),
+              onChanged: (double? value) {
+                notifiers[key]?.value =
+                    value ?? juceDropdownParameters[key]!.init;
+              },
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+
+  /// Builds a checkbox for a parameter that is coppled with a [ValueNotifier].
+  /// The checkbox is configured as specified in the [juceCheckboxParameters].
+  ValueListenableBuilder<bool>? checkboxBuilder(String key) {
+    final ({bool init, String title})? parameter = juceCheckboxParameters[key];
+    if (parameter == null) return null;
+    return ValueListenableBuilder<bool>(
+      valueListenable: notifiers[key]! as ValueNotifier<bool>,
+      builder: (BuildContext context, bool value, Widget? child) =>
+          CheckboxListTile(
+        title: Text(parameter.title),
+        value: value,
+        onChanged: (bool? value) {
+          notifiers[key]?.value = value ?? juceCheckboxParameters[key]!.init;
+        },
+      ),
+    );
+  }
+
+  /// Selects the right builder for a parameter.
+  ValueListenableBuilder<Object>? selectBuilder(String key) {
+    if (juceSliderParameters.containsKey(key)) {
+      return sliderBuilder(key);
+    } else if (juceDropdownParameters.containsKey(key)) {
+      return dropdownBuilder(key);
+    } else if (juceCheckboxParameters.containsKey(key)) {
+      return checkboxBuilder(key);
+    }
+    return null;
+  }
+
   @override
   Future<void> dispose() async {
     // cleanup value notifiers
@@ -155,8 +379,10 @@ class _ConfiguratorScreenState extends State<ConfiguratorScreen> {
             ),
             children: <Widget>[
               const DeviceVolumeSlider(),
-              for (final String key in juceSliderParameters.keys)
-                sliderBuilder(key) ?? const SizedBox.shrink(),
+              for (final String key in notifiers.keys) ...<Widget>[
+                selectBuilder(key) ?? const SizedBox.shrink(),
+                const SmallPadder(),
+              ],
             ],
           ),
         ),
